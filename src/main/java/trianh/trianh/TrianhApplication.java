f70036c6324d7e41ae9565e9d9c16e373ca49bbc
Le Huu Tri Anh <anhnguyen8761@gmail.com>
2026-03-26 15:19:21 +0700

    Add docker build/push workflow

---
+++ src/main/java/trianh/trianh/TrianhApplication.java
@@ -0,0 +1,13 @@
+package trianh.trianh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrianhApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrianhApplication.class, args);
+	}
+
+}
---
+++ src/test/java/trianh/trianh/TrianhApplicationTests.java
@@ -0,0 +1,13 @@
+package trianh.trianh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrianhApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
